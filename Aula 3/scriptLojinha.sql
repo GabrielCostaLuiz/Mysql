411488ec4104407435f39442e17ad9416478aa9a
GabrielCostaLuiz <53578058+GabrielCostaLuiz@users.noreply.github.com>
2020-05-04 14:44:47 -0300

    Update scriptLojinha.sql

--- Aula 3/scriptLojinha.sql
+++ Aula 3/scriptLojinha.sql
@@ -30,7 +30,7 @@ create table tbprodutos(
 	idpro int primary key auto_increment,
     produto varchar(50) not null,
     valor decimal(10,2),
-    quantidade varchar(15) not null
+    quantidade int not null
 );
 
 describe tbprodutos;
